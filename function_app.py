f852d3f03a9ded32063ccabfebbcc9beb7ef8aaa
gapanovichal@gmail.com <gapanovichal@gmail.com>
2024-03-25 19:12:40 -0700

    remove dev env

--- function_app.py
+++ function_app.py
@@ -3,7 +3,6 @@ import datetime
 import rss_syphon as syphon
 import rss_data as data
 import rss_config as config
-from splunk import *
 from slacker import *
 from zendesk import *
 from notion import *
