071f4950e6109d737b570eac9dfd87de09017c5e
gapanovichal@gmail.com <gapanovichal@gmail.com>
2024-04-10 15:14:32 -0700

    hardcoded 5

--- rss_config.py
+++ rss_config.py
@@ -12,8 +12,8 @@ log_format = os.getenv("LOG_FORMAT", "%(levelname)s:%(name)s: %(message)s")
 slack_params_dict = {
     "slack_enabled": os.getenv("SLACK_ENABLED"),
     "slack_token": os.getenv("SLACK_TOKEN"),
-#    "pages_to_read": int(os.getenv("SLACK_PAGES_TO_READ")),
-    pages_to_read = 5,
+    "pages_to_read": ("SLACK_PAGES_TO_READ", 5),
+    
     "channels": {
         "cve": os.getenv("SLACK_CHANNEL_CVE"),
         "news": os.getenv("SLACK_CHANNEL_NEWS"),
