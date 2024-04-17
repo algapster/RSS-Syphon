94647a376344f87ce66236578c2a016eae40e114
gapanovichal@gmail.com <gapanovichal@gmail.com>
2024-04-17 11:11:05 -0700

    ZDI removal

--- rss_data.py
+++ rss_data.py
@@ -72,8 +72,6 @@ rss_feed_list = {
         {"name": "Windows Internals Blog", "url": "https://windows-internals.com/feed/"},
         {"name": "nist-analyzed", "url": "https://nvd.nist.gov/feeds/xml/cve/misc/nvd-rss-analyzed.xml"},
         {"name": "nist-upcoming", "url": "https://nvd.nist.gov/feeds/xml/cve/misc/nvd-rss.xml"},
-        {"name": "zdi-upcoming", "url": "https://www.zerodayinitiative.com/rss/upcoming/"},
-        {"name": "zdi-analyzed", "url": "https://www.zerodayinitiative.com/rss/published/"},
         {"name": "vulners", "url": "https://vulners.com/rss.xml"},
         {"name": "seclists-bugtraq", "url": "https://seclists.org/rss/bugtraq.rss"},
         {"name": "seclists-full", "url": "https://seclists.org/rss/fulldisclosure.rss"},
